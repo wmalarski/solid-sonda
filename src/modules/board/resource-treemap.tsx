447e3d83fbec940c154d2c87debe1a3494af796a
Wojtek Malarski <w.malarski@gmail.com>
2026-03-10 19:50:33 +0100

    packages bump

--- src/modules/board/resource-treemap.tsx
+++ src/modules/board/resource-treemap.tsx
@@ -8,11 +8,30 @@ type ResourcesTreemapProps = {
 };
 
 export const ResourcesTreemap: Component<ResourcesTreemapProps> = (props) => {
+  const width = 100;
+  const height = 100;
+
+  // const dd = createMemo(() => {
+  //   d3.hierarchy()
+
+  //   return null;
+  // })
+
+  // const hierarchy = d3
+  //   .hierarchy(props.data)
+  //   .sum((d) => d.value)
+  //   .toSorted((a, b) => b.value - a.value);
+
+  // d3.treemap().size([width, height]).paddingOuter(3).paddingTop(19).paddingInner(1).round(true)(
+  //   hierarchy,
+  // );
+
   return (
     <div>
       <pre>{JSON.stringify(props.asset, null, 2)}</pre>
       <pre>{JSON.stringify(props.children, null, 2)}</pre>
       <pre>{JSON.stringify(props.resource, null, 2)}</pre>
+      <svg class="w-full h-full z-10 isolate"></svg>
     </div>
   );
 };
